a189e4de1ed229b446d1d22403753b52a827f399
Michael Carr <xyeres@me.com>
2021-01-16 19:35:07 -0800

    Assign localstorage var to the field value instead of label value

--- src/js/waitlist.js
+++ src/js/waitlist.js
@@ -1,5 +1,5 @@
 //populate email if already exists
 if (sessionStorage.email) {
-    document.getElementById('email-id').innerHTML = sessionStorage.email
+    document.getElementById('email-id').value = sessionStorage.email
 } else if (localStorage.email) {
-     document.getElementById('email-id').innerHTML = localStorage.email }
+     document.getElementById('email-id').value = localStorage.email }
